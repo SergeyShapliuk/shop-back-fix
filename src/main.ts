168793ce4c248f113b0ccd0e8be731c80872c592
SergeyShapliuk <sergeshapluk@gmail.com>
2022-05-26 13:12:49 +0300

    startServer

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,46 @@
-import { ApolloServer } from 'apollo-server';
-
+import {createServer} from "http";
 import typeDefs from './schema';
 import resolvers from './resolvers';
+import {ApolloServer} from 'apollo-server-express';
 
-const server = new ApolloServer({
-    typeDefs: typeDefs,
-    resolvers: resolvers
-});
+const express = require("express")
+// const cors = require("cors")
+const app = express()
+const httpServer = createServer(app)
+// const path = require("path")
+const port = process.env.PORT || 4000
+// const whitelist = ["http://localhost:4000"]
+// const corsOptions = {
+//     origin: function (origin: any, callback: any) {
+//         if (!origin || whitelist.indexOf(origin) !== -1) {
+//             callback(null, true)
+//         } else {
+//             callback(new Error("Not allowed by CORS"))
+//         }
+//     },
+//     credentials: true,
+// }
+// app.use(cors(corsOptions))
+const startServer = async () => {
+    const app = express()
+    const httpServer = createServer(app)
+    const server = new ApolloServer({
+        typeDefs: typeDefs,
+        resolvers: resolvers,
+    });
+    await server.start()
+    server.applyMiddleware({app, path: '/'})
+    httpServer.listen({port}, () =>
+        console.log(`Server listening on localhost:4000${server.graphqlPath}`))
+}
+startServer()
 
-server.listen().then(({ url }) => {
-    console.log(`🚀  Server ready at ${url}`);
-});
+//
+//
+// app.get("/", (req: any, res: any) => {
+//     res.send({message: "Hello World!"});
+// });
+//
+// app.listen().then({port},() => {
+//     console.log(`🚀  Server ready at ${port}`);
+// });
