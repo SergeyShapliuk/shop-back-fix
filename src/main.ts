7552928fc05a55ea2a0ab363fe99d35abe2d5510
SergeyShapliuk <Sergeshapluk@gmail.com>
2022-06-07 16:36:45 +0300

    fix: main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
-import { ApolloServer } from 'apollo-server';
-
+import { ApolloServer } from 'apollo-server-express';
+import express from 'express';
 import typeDefs from './schema';
 import resolvers from './resolvers';
 
