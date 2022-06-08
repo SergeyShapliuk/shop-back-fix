03b62ed95943749a8e60482f816aafb0efae4011
SergeyShapliuk <Sergeshapluk@gmail.com>
2022-06-08 12:23:47 +0300

    fix: main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,16 @@
-import { ApolloServer } from 'apollo-server-express';
-import express from 'express';
+import { ApolloServer } from 'apollo-server';
 import typeDefs from './schema';
 import resolvers from './resolvers';
 
-const app = express();
 const server = new ApolloServer({
     typeDefs: typeDefs,
     resolvers: resolvers,
     introspection: true,
     playground: true,
 });
-server.applyMiddleware({ app });
-app.get('/', (req, res) => {
-  res.send('hello');
-});
-const PORT = process.env.PORT || 4000;
-app.listen(PORT,()=> {
-    console.log(`🚀  Server ready at ${PORT}`);
+server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
+  console.log(`
+      Server is ready at ${url}
+      Query at https://studio.apollographql.com/dev
+  `);
 });
